1b14fbfdf2d26576906553b9247e00b6ae3fb01b
BLOSSOM1994 <46196923+BLOSSOM1994@users.noreply.github.com>
2021-07-20 02:53:03 +0430

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -9,50 +9,52 @@ import tensorflow as tf
 
 import pprint
 import os
+import pandas as pd
 
-flags = tf.app.flags
-flags.DEFINE_integer("epoch", 120, "Number of epoch [120]")
-flags.DEFINE_integer("batch_size", 1, "The size of batch images [128]")
-flags.DEFINE_integer("image_height", 112, "The size of image to use [230]")
-flags.DEFINE_integer("image_width", 112, "The size of image to use [310]")
-flags.DEFINE_integer("label_height", 112, "The size of label to produce [230]")
-flags.DEFINE_integer("label_width", 112, "The size of label to produce [310]")
-flags.DEFINE_float("learning_rate", 0.001, "The learning rate of gradient descent algorithm [1e-4]")
-flags.DEFINE_float("beta1", 0.5, "Momentum term of adam [0.5]")
-flags.DEFINE_integer("c_dim", 3, "Dimension of image color. [3]")
-flags.DEFINE_integer("c_depth_dim", 1, "Dimension of depth. [1]")
-flags.DEFINE_string("checkpoint_dir", "checkpoint", "Name of checkpoint directory [checkpoint]")
-flags.DEFINE_string("sample_dir", "sample", "Name of sample directory [sample]")
-flags.DEFINE_string("test_data_dir", "test", "Name of sample directory [test]")
-flags.DEFINE_boolean("is_train", False, "True for training, False for testing [True]")
-FLAGS = flags.FLAGS
-
-pp = pprint.PrettyPrinter()
 
 def main(_):
-  pp.pprint(flags.FLAGS.__flags)
+  checkpoint_dir='/content/drive/MyDrive/DataSet_WaterNet/checkpoint'
+  sample_dir='/content/drive/MyDrive/DataSet_WaterNet/challenging-60'
+  FLAGS=pd.DataFrame()
+  FLAGS["epoch"]=[4]
+  FLAGS["batch_size"]=[128]
+  FLAGS["image_height"] = [ 112]
+  FLAGS["image_width"] = [ 112]
+  FLAGS["label_height"] = [ 112]
+  FLAGS["label_width"] = [ 112]
+  FLAGS["learning_rate"] = [ 0.001]
+  FLAGS["beta1"] = [ 0.5]
+  FLAGS["c_dim"] = [ 3]
+  FLAGS["checkpoint_dir"] = [ '/content/drive/MyDrive/DataSet_WaterNet/checkpoint']
+  FLAGS["sample_dir"] = [ '/content/drive/MyDrive/DataSet_WaterNet/challenging-60']
+  FLAGS["test_data_dir"] = [ "/content/drive/MyDrive/DataSet_WaterNet/output"]
+  FLAGS["is_train"] = [ False]
+
+
+
+
 
   if not os.path.exists(FLAGS.checkpoint_dir):
     os.makedirs(FLAGS.checkpoint_dir)
   if not os.path.exists(FLAGS.sample_dir):
     os.makedirs(FLAGS.sample_dir)
-  filenames = os.listdir('test_real')
-  data_dir = os.path.join(os.getcwd(), 'test_real')
+  filenames = os.listdir('/content/drive/MyDrive/DataSet_WaterNet/test_real')
+  data_dir = os.path.join(os.getcwd(), '/content/drive/MyDrive/DataSet_WaterNet/test_real')
   data = glob.glob(os.path.join(data_dir, "*.png"))
   test_data_list = data + glob.glob(os.path.join(data_dir, "*.jpg"))+glob.glob(os.path.join(data_dir, "*.bmp"))+glob.glob(os.path.join(data_dir, "*.jpeg"))
 
-  filenames1 = os.listdir('wb_real')
-  data_dir1 = os.path.join(os.getcwd(), 'wb_real')
+  filenames1 = os.listdir('/content/drive/MyDrive/DataSet_WaterNet/wb_real')
+  data_dir1 = os.path.join(os.getcwd(), '/content/drive/MyDrive/DataSet_WaterNet/wb_real')
   data1 = glob.glob(os.path.join(data_dir1, "*.png"))
   test_data_list1 = data1 + glob.glob(os.path.join(data_dir1, "*.jpg"))+glob.glob(os.path.join(data_dir1, "*.bmp"))+glob.glob(os.path.join(data_dir1, "*.jpeg"))
 
-  filenames2 = os.listdir('ce_real')
-  data_dir2 = os.path.join(os.getcwd(), 'ce_real')
+  filenames2 = os.listdir('/content/drive/MyDrive/DataSet_WaterNet/ce_real')
+  data_dir2 = os.path.join(os.getcwd(), '/content/drive/MyDrive/DataSet_WaterNet/ce_real')
   data2 = glob.glob(os.path.join(data_dir2, "*.png"))
   test_data_list2 = data2 + glob.glob(os.path.join(data_dir2, "*.jpg"))+glob.glob(os.path.join(data_dir2, "*.bmp"))+glob.glob(os.path.join(data_dir2, "*.jpeg"))
 
-  filenames3 = os.listdir('gc_real')
-  data_dir3 = os.path.join(os.getcwd(), 'wb_real')
+  filenames3 = os.listdir('/content/drive/MyDrive/DataSet_WaterNet/gc_real')
+  data_dir3 = os.path.join(os.getcwd(), '/content/drive/MyDrive/DataSet_WaterNet/gc_real')
   data3 = glob.glob(os.path.join(data_dir3, "*.png"))
   test_data_list3 = data3 + glob.glob(os.path.join(data_dir3, "*.jpg"))+glob.glob(os.path.join(data_dir3, "*.bmp"))+glob.glob(os.path.join(data_dir3, "*.jpeg"))
 
